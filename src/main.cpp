ee57d8eb7c73acbc3592992d80dc4122245950d6
Servio Palacios <servio@palacios.com>
2023-05-21 11:33:25 -0400

    [first commit]

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+
+    std::cout << "==== The Simulation process was completed successfully ===="
+              << std::endl;
+
+    return 0;
+} // main
